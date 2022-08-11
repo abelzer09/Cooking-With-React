ffa1130653434c10276cc25683fb9b97090e157a
abelzer09 <aaronrbelzer@gmail.com>
2022-08-11 15:01:13 -0500

    added css styling to buttons and added some font styling

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -11,11 +11,11 @@ export default function Recipe(props) {
     } = props
   return (
     <div className="recipe">
-      <div>
-        <h3>{name}</h3>
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
         <div>
-            <button>Edit</button>
-            <button>Delete</button>
+            <button className='btn btn--primary mr-1'>Edit</button>
+            <button className='btn btn-danger'>Delete</button>
         </div>
       </div>
       <div>
