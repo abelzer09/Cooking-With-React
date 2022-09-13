4a2567d501804ebd34811924687d9918e8408a04
abelzer09 <aaronrbelzer@gmail.com>
2022-09-13 11:12:27 -0500

    changed to context from props

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import IngredientList from './IngredientList'
+import { RecipeContext } from './App'
 
 export default function Recipe(props) {
-    const {
-        name, 
-        cookTime, 
-        servings, 
-        instructions,
-        ingredients
-    } = props
+  const { handleRecipeDelete } = useContext(RecipeContext)
+  const {
+    id,
+    name,
+    cookTime,
+    servings,
+    instructions,
+    ingredients
+  } = props
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
         <div>
-            <button className='btn btn--primary mr-1'>Edit</button>
-            <button className='btn btn--danger'>Delete</button>
+          <button className='btn btn--primary mr-1'>Edit</button>
+          <button
+            className='btn btn--danger'
+            onClick={() => handleRecipeDelete(id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
       <div className='recipe__row'>
@@ -29,13 +37,13 @@ export default function Recipe(props) {
       <div className='recipe__row'>
         <span className='recipe__label'>Instructions:</span>
         <div className='recipe__value recipe__instructions recipe__value--indented'>
-            {instructions}
+          {instructions}
         </div>
       </div>
       <div className='recipe__row'>
         <span className='recipe__label'>Ingredients:</span>
         <div className='recipe__value recipe__value--indented'>
-            <IngredientList ingredients={ingredients} />
+          <IngredientList ingredients={ingredients} />
         </div>
       </div>
     </div>
